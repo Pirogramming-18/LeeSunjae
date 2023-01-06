4ab652d6ecdc35862554b2e9bdb20f1826eec736
sunjae <zxsunje775@naver.com>
2023-01-07 02:14:05 +0900

    1-9 만세~~~

--- python_problem/python_problem1.py
+++ python_problem/python_problem1.py
@@ -1,3 +1,4 @@
+import random
 
 def brGame() :
     num = 0
@@ -7,39 +8,43 @@ def brGame() :
         turn += 1
 
         if turn % 2 == 1:
-            first = 'A'
-            second = 'B'
+            first = 'computer'
+            second = 'player'
         else:
-            first = 'B'
-            second = 'A'
-            
+            first = 'player'
+            second = 'computer'
+
         while True:
-            
-            n = input('부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능) : ')
+
+
             try:
-                n = int(n)
+                if first == 'computer':
+                    n = random.randint(1,3)
+                else:
+                    n = input('부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능) : ')
+                    n = int(n)
             except ValueError:
                 print('정수를 입력하세요')
             else:
                 if 1 <= n <= 3:
                     if 31 <= num+n:
                         for i in range(num,31):
-                            print(f'player{first} : {i+1}')
+                            print(f'{first} : {i+1}')
                         num += n
                     else:
                         for i in range(num,num+n):
-                            print(f'player{first} : {i+1}')
+                            print(f'{first} : {i+1}')
                         num += n
                     break
                 else:
                     print('1,2,3 중 하나를 입력하세요')
 
         if 31 <= num:
-            print(f'Player{second} Win!')
+            print(f'{second} Win!')
             break
 
-if __name__ == "__main__":
-    brGame()
+# if __name__ == "__main__":
+brGame()
 
     # while True:
     #     n = input('부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능) : ')
