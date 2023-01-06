006ff787470d42fd93b976a2f43083144ff08007
sunjae <zxsunje775@naver.com>
2023-01-07 01:35:24 +0900

    1-8

--- python_problem/python_problem1.py
+++ python_problem/python_problem1.py
@@ -1,50 +1,66 @@
-num = 0
 
-while True:
-    while True:
-        n = input('부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능) : ')
-        try:
-            n = int(n)
-        except ValueError:
-            print('정수를 입력하세요')
-        else:
-            if 1 <= n <= 3:
-                if 31 <= num+n:
-                    for i in range(num,31):
-                        print(f'playerA : {i+1}')
-                    num += n
-                else:
-                    for i in range(num,num+n):
-                        print(f'playerA : {i+1}')
-                    num += n
-                break
-            else:
-                print('1,2,3 중 하나를 입력하세요')
-
-    if 31 <= num:
-        print('PlayerB Win!')
-        break
+def brGame() :
+    num = 0
+    turn = 0
 
     while True:
-        n_2 = input('부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능) : ')
-        try:
-            n_2 = int(n_2)
-        except ValueError:
-            print('정수를 입력하세요')
-        else:
-            if 1 <= n_2 <= 3:
-                if 31 <= num+n_2:
-                    for i in range(num,31):
-                        print(f'playerB : {i+1}')
-                    num += n_2
-                else:
-                    for i in range(num,num + n_2):
-                        print(f'playerB : {i+1}')
-                    num += n_2
-                break
-            else:
-                print('1,2,3 중 하나를 입력하세요')
+        turn += 1
 
-    if 31 <= num:
-        print('PlayerA win!')
-        break
+        if turn % 2 == 1:
+            first = 'A'
+            second = 'B'
+        else:
+            first = 'B'
+            second = 'A'
+            
+        while True:
+            
+            n = input('부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능) : ')
+            try:
+                n = int(n)
+            except ValueError:
+                print('정수를 입력하세요')
+            else:
+                if 1 <= n <= 3:
+                    if 31 <= num+n:
+                        for i in range(num,31):
+                            print(f'player{first} : {i+1}')
+                        num += n
+                    else:
+                        for i in range(num,num+n):
+                            print(f'player{first} : {i+1}')
+                        num += n
+                    break
+                else:
+                    print('1,2,3 중 하나를 입력하세요')
+
+        if 31 <= num:
+            print(f'Player{second} Win!')
+            break
+
+if __name__ == "__main__":
+    brGame()
+
+    # while True:
+    #     n = input('부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능) : ')
+    #     try:
+    #         n = int(n)
+    #     except ValueError:
+    #         print('정수를 입력하세요')
+    #     else:
+    #         if 1 <= n <= 3:
+    #             if 31 <= num+n:
+    #                 for i in range(num,31):
+    #                     print(f'player{first} : {i+1}')
+    #                 num += n
+    #             else:
+    #                 for i in range(num,num + n):
+    #                     print(f'player{first} : {i+1}')
+    #                 num += n
+    #             break
+    #         else:
+    #             print('1,2,3 중 하나를 입력하세요')
+
+    # if 31 <= num:
+    #     print(f'Player{second} win!')
+    #     break
