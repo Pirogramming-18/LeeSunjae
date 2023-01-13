b099faa3d19e28d39d1163a5255b06a95b4b6ac5
sunjae <zxsunje775@naver.com>
2023-01-13 22:29:22 +0900

    무비과제완성

--- MyMovieReviews/myapp/views.py
+++ MyMovieReviews/myapp/views.py
@@ -5,14 +5,16 @@ from django.views.decorators.csrf import csrf_exempt
 
 nextId = 4
 topics = [
-    {'id':1, 'title':'루카', 'body':'루카 내용', 'year':'2021', 'score':97, 'genre':'drama' ,'director':'엔리코 카사로사' ,'time':'100', 'actor':'루카'},
-    {'id':2, 'title':'소울', 'body':'소울 내용','year':'2021', 'score':100, 'genre':'life','director':'피트 닥터' ,'time':'95', 'actor':'조,22'},
-    {'id':3, 'title':'라야와 마지막 드래곤', 'body':'라야 내용', 'year':'2021', 'score':95, 'genre':'Humanity', 'director':'돈 홀' ,'time':'120', 'actor':'라야,시수'},
+    {'id':1, 'title':'루카', 'body':'형제애와 우애를 느낄 수 있었습니다 픽사의 우정이야기', 'year':'2021', 'score':97, 'genre':'drama' ,'director':'엔리코 카사로사' ,'time':'100', 'actor':'루카'},
+    {'id':2, 'title':'소울', 'body':'삶에서 진정 중요한 것이 무엇인지 느낄 수 있었던 이선재 인생 영화 2위','year':'2021', 'score':100, 'genre':'life','director':'피트 닥터' ,'time':'95', 'actor':'조,22'},
+    {'id':3, 'title':'라야와 마지막 드래곤', 'body':'돌처럼 굳은 서로를 되돌릴 수 있는 것은 그보다 굳건한 진심', 'year':'2021', 'score':95, 'genre':'Humanity', 'director':'돈 홀' ,'time':'120', 'actor':'라야,시수'},
 ]
 
 def HTMLTemplate(articleTag, id=None):
     global topics
-    contextUI = ''
+    contextUI = f'''
+            <li><a href="/create/">리뷰 작성</a></li>
+    '''
     if id != None:
         contextUI = f'''
             <li>
@@ -30,12 +32,8 @@ def HTMLTemplate(articleTag, id=None):
     <html>
     <body>
         <h1><a href="/">MYMovieReviews</a></h1>
-        <ol>
-            {ol}
-        </ol>
         {articleTag}
         <ul>
-            <li><a href="/create/">리뷰 생성하기</a></li>
             {contextUI}
         </ul>
     </body>
@@ -43,9 +41,16 @@ def HTMLTemplate(articleTag, id=None):
     '''
 
 def index(request):
-    article = '''
-    <h2>영화리뷰입니다</h2>
-    리뷰는 생성,삭제,수정 가능합니다
+    global topics
+    ol = ''
+    for topic in topics:
+        ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a><br>개봉 년도 : {topic["year"]}<br>평점 : {topic["score"]}<br>장르 : {topic["genre"]}</li>'
+    article = f'''
+    <ol>
+        {ol}
+    </ol>
+    <h2>영화리뷰 리스트페이지입니다</h2>
+    리뷰는 해당 페이지를 통해 생성 가능합니다
     '''
     return HttpResponse(HTMLTemplate(article))
 
@@ -54,7 +59,7 @@ def read(request, id):
     article = ''
     for topic in topics:
         if topic['id'] == int(id):
-            article = f'<h2>{topic["title"]}</h2>{topic["body"]}<br>감독 : {topic["director"]}<br>러닝타임 : {topic["time"]}<br>출연진 : {topic["actor"]}'
+            article = f'<h2>{topic["title"]} 디테일 페이지입니다<h2><h4>{topic["title"]}</h4>{topic["body"]}<br>감독 : {topic["director"]}<br>러닝타임 : {topic["time"]}<br>출연진 : {topic["actor"]}'
     return HttpResponse(HTMLTemplate(article, id))
 
 @csrf_exempt
@@ -62,6 +67,7 @@ def create(request):
     global nextId
     if request.method == 'GET':
         article = '''
+        <h2>리뷰 생성 페이지입니다<h2>
             <form action="/create/" method="post">
                 <p><input type="text" name="title" placeholder="영화제목"></p>
                 <p><input type="text" name="year" placeholder="개봉년도"></p>
@@ -71,7 +77,7 @@ def create(request):
                 <p><input type="text" name="time" placeholder="러닝타임"></p>
                 <p><input type="text" name="actor" placeholder="출연"></p>
                 <p><textarea name="body" placeholder="영화 리뷰"></textarea></p>
-                <p><input type="submit"></p>
+                <p><input type="submit" value="저장"></p>
             </form>
         '''
         return HttpResponse(HTMLTemplate(article))
@@ -88,7 +94,7 @@ def create(request):
         url = '/read/' + str(nextId)
         nextId = nextId + 1
         topics.append(newTopic)
-        return redirect(url)
+        return redirect('/')
 
 @csrf_exempt
 def delete(request):
@@ -119,6 +125,7 @@ def update(request,id):
                     "actor":topic['actor']
                 }
         article = f'''
+        <h2>리뷰 수정 페이지입니다</h2>
             <form action="/update/{id}/" method="post">
                 <p><input type="text" name="title" placeholder="title" value={selectedTopic["title"]}></p>
                 <p><input type="text" name="year" placeholder="year" value={selectedTopic["year"]}></p>
