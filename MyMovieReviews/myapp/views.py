f16155dd618568398c0658c3ff9b9d2eb0e78b4a
sunjae <zxsunje775@naver.com>
2023-01-13 21:10:03 +0900

    무비과제저장용3(생성기능미완)

--- MyMovieReviews/myapp/views.py
+++ MyMovieReviews/myapp/views.py
@@ -5,9 +5,9 @@ from django.views.decorators.csrf import csrf_exempt
 
 nextId = 4
 topics = [
-    {'id':1, 'title':'루카', 'body':'루카 내용'},
-    {'id':2, 'title':'소울', 'body':'소울 내용'},
-    {'id':3, 'title':'라야와 마지막 드래곤', 'body':'라야 내용'},
+    {'id':1, 'title':'루카', 'body':'루카 내용', 'year':'2021', 'score':97, 'genre':'drama' ,'director':'엔리코 카사로사' ,'time':'100', 'actor':'루카'},
+    {'id':2, 'title':'소울', 'body':'소울 내용','year':'2021', 'score':100, 'genre':'life','director':'피트 닥터' ,'time':'95', 'actor':'조,22'},
+    {'id':3, 'title':'라야와 마지막 드래곤', 'body':'라야 내용', 'year':'2021', 'score':95, 'genre':'Humanity', 'director':'돈 홀' ,'time':'120', 'actor':'라야,시수'},
 ]
 
 def HTMLTemplate(articleTag, id=None):
@@ -18,14 +18,14 @@ def HTMLTemplate(articleTag, id=None):
             <li>
                 <form action="/delete/" method="post">
                     <input type="hidden" name="id" value={id}>
-                    <input type="submit" value="삭제하기">
+                    <input type="submit" value="현재리뷰 삭제하기">
                 </form>
             </li>
-            <li><a href="/update/{id}">수정하기</a></li>
+            <li><a href="/update/{id}">현재리뷰 수정하기</a></li>
         '''
     ol = ''
     for topic in topics:
-        ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
+        ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a><br>개봉 년도 : {topic["year"]}<br>평점 : {topic["score"]}<br>장르 : {topic["genre"]}</li>'
     return f'''
     <html>
     <body>
@@ -35,7 +35,7 @@ def HTMLTemplate(articleTag, id=None):
         </ol>
         {articleTag}
         <ul>
-            <li><a href="/create/">생성하기</a></li>
+            <li><a href="/create/">리뷰 생성하기</a></li>
             {contextUI}
         </ul>
     </body>
@@ -54,7 +54,7 @@ def read(request, id):
     article = ''
     for topic in topics:
         if topic['id'] == int(id):
-            article = f'<h2>{topic["title"]}</h2>{topic["body"]}'
+            article = f'<h2>{topic["title"]}</h2>{topic["body"]}<br>감독 : {topic["director"]}<br>러닝타임 : {topic["time"]}<br>출연진 : {topic["actor"]}'
     return HttpResponse(HTMLTemplate(article, id))
 
 @csrf_exempt
@@ -63,8 +63,8 @@ def create(request):
     if request.method == 'GET':
         article = '''
             <form action="/create/" method="post">
-                <p><input type="text" name="title" placeholder="title"></p>
-                <p><textarea name="body" placeholder="body"></textarea></p>
+                <p><input type="text" name="title" placeholder="영화제목"></p>
+                <p><textarea name="body" placeholder="영화 리뷰"></textarea></p>
                 <p><input type="submit"></p>
             </form>
         '''
